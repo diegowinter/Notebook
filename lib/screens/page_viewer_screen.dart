a426ee78f396637f5e2fb4736eab5d0785cbfe05
Diego Winter <diego@diegowinter.dev>
2021-08-05 21:49:48 -0300

    change dialog to confirmationDialog from Dialogs class

--- lib/screens/page_viewer_screen.dart
+++ lib/screens/page_viewer_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:markdown/markdown.dart' as md;
 import 'package:notebook/providers/pages.dart';
+import 'package:notebook/utils/Dialogs.dart';
 import 'package:provider/provider.dart';
 
 enum ItemOptions {
@@ -12,32 +13,16 @@ enum ItemOptions {
 class PageViewerScreen extends StatelessWidget {
   const PageViewerScreen({ Key? key }) : super(key: key);
 
-  Future<bool> _showDeleteConfirmationDialog(BuildContext context) async {
-    return await showDialog(
-      context: context,
-      builder: (_) => AlertDialog(
-        title: Text('Excluir página'),
-        content: Text('Tem certeza que deseja excluir esta página?'),
-        actions: [
-          TextButton(
-            child: Text('Não'),
-            onPressed: () => Navigator.of(context).pop(false),
-          ),
-          TextButton(
-            child: Text('Sim'),
-            onPressed: () => Navigator.of(context).pop(true),
-          )
-        ],
-      )
-    );
-  }
-
   Future<void> _editPage() async {
     print('edit...');
   }
 
   Future<void> _deletePage(BuildContext context, String pageId) async {
-    bool confirmation = await _showDeleteConfirmationDialog(context);
+    bool confirmation = await Dialogs.confirmationDialog(
+      context: context,
+      title: 'Excluir página',
+      content: 'Tem certeza que deseja excluir esta página?'
+    );
     if (confirmation) {
       Provider.of<Pages>(context, listen: false).deletePage(pageId);
       Navigator.of(context).pop();
