89edaa966e0a83c27674820c23c60e71f0b0d883
Diego Winter <diego@diegowinter.dev>
2021-08-09 23:37:33 -0300

    update constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
-  static const FIREBASE_URL = 'https://notebook-77031-default-rtdb.firebaseio.com';
+  static const FIREBASE_URL = 'FIREBASE_REALTIME_DATABASE_URL';
   static const FIREBASE_AUTH_URL = 'https://identitytoolkit.googleapis.com/v1/accounts';
   static const FIREBASE_REFRESH_TOKEN_URL = 'https://securetoken.googleapis.com/v1/token';
-  static const FIREBASE_API_KEY = 'AIzaSyBEOwKC9RRJMZ8HcbJpo0GY8OpavGS-Jmg';
+  static const FIREBASE_API_KEY = 'FIREBASE_API_KEY';
 }
